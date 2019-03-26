b3d4ea6abfd294575f8d2e05dad13334cc2f5348
Nick <nk.chikhladze@gmail.com>
2019-03-26 13:19:48 +0400

    Revert "Getting ready for release test #2."
    This reverts commit 5edc0c38

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SfgPetClinicApplicationTest {
+
+	@Test
+	public void main() {
+	}
+}
