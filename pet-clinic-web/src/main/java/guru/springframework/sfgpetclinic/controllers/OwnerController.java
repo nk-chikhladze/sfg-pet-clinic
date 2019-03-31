769a4f84adbd542eba08ec4d759b5054d9fd6266
Nick <nk.chikhladze@gmail.com>
2019-03-31 14:10:44 +0400

    Added MockMvc Tests for Owner Controller. Closes #52

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -15,7 +15,7 @@ public class OwnerController {
 		this.ownerService = ownerService;
 	}
 
-	@RequestMapping({"", "/", "/index"})
+	@RequestMapping({"", "/", "/index", "/index.html"})
 	public String listOwners(Model model){
 		model.addAttribute("owners", ownerService.findAll());
 		return "owners/index";
